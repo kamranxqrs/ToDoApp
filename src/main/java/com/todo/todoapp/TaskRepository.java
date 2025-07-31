5fb619c57fb9b29ff6e668aff9986365cf8a9d9c
Kamran Qureshi <kamranquraishi801@gmail.com>
2025-07-31 23:36:43 +0530

    Initial commit

---
+++ src/main/java/com/todo/todoapp/TaskRepository.java
@@ -0,0 +1,7 @@
+package com.todo.todoapp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    
+}
---
+++ src/main/java/com/todo/todoapp/TaskService.java
@@ -0,0 +1,26 @@
+package com.todo.todoapp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
+
